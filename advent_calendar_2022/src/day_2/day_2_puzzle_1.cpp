43f8b846504b25ae8159a7c8ea6d0bd14687e726
Allen Liu <aliu@nauticusrobotics.com>
2023-02-06 13:28:24 -0600

    Created new function for symbol points

--- advent_calendar_2022/src/day_2/day_2_puzzle_1.cpp
+++ advent_calendar_2022/src/day_2/day_2_puzzle_1.cpp
@@ -7,6 +7,22 @@ using namespace std;
 
 enum symbol {X=1,Y,Z};
 
+
+void symbol_points(map<char, int>::iterator &key_find, int &total_score){
+    switch(key_find->second){
+        case 1:
+            total_score +=1;
+            break;
+        case 2:
+            total_score +=2;
+            break;
+        case 3:
+            total_score +=3;
+            break;
+    }
+
+}
+
 int main(){
     string line;
     symbol symbol;
@@ -26,48 +42,18 @@ int main(){
             if ((opponent_symbol == 'A' && my_symbol == 'X') or (opponent_symbol == 'B' && my_symbol == 'Y') 
             or (opponent_symbol == 'C' && my_symbol == 'Z')){
                 total_score += 3;
-                switch(key_find->second){
-                    case 1:
-                        total_score +=1;
-                        break;
-                    case 2:
-                        total_score +=2;
-                        break;
-                    case 3:
-                        total_score +=3;
-                        break;
-                }
+                symbol_points(key_find, total_score);
 
             }
             else if ((opponent_symbol == 'A' && my_symbol == 'Y') or (opponent_symbol == 'B' && my_symbol == 'Z') 
                     or (opponent_symbol == 'C' && my_symbol == 'X')){
                 total_score += 6;
-                switch(key_find->second){
-                    case 1:
-                        total_score +=1;
-                        break;
-                    case 2:
-                        total_score +=2;
-                        break;
-                    case 3:
-                        total_score +=3;
-                        break;
-                }
+                symbol_points(key_find, total_score);
+
                             
             }
             else{
-                switch(key_find->second){
-                    case 1:
-                        total_score +=1;
-                        break;
-                    case 2:
-                        total_score +=2;
-                        break;
-                    case 3:
-                        total_score +=3;
-                        break;
-                }
-
+                symbol_points(key_find, total_score);
             }
         }
         cout << "My score is: " << total_score;
