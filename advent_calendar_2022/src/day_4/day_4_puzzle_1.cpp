6fdae52a1bbfe2351a2fe31f1ed563e0257af8ba
Allen Liu <aliu@nauticusrobotics.com>
2023-03-08 12:56:40 -0600

    Added more tests, implemented main function

--- advent_calendar_2022/src/day_4/day_4_puzzle_1.cpp
+++ advent_calendar_2022/src/day_4/day_4_puzzle_1.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <cstring>
 #include <algorithm>
+#include <fstream>
 
 using namespace std;
 
@@ -35,12 +36,12 @@ vector<int> second_range = {};
     }
     split_strings.push_back(first_half);
     split_strings.push_back(sec_half);
-    cout << split_strings[0] << ";" << split_strings[1] << endl;
+    // cout << split_strings[0] << ";" << split_strings[1] << endl;
 
     return split_strings;
 }
 
-// This function takes in range in string form and a vector, 
+// This function takes in a range in string form and a vector, 
 // then creates an inclusive sequence with the specified vector
 void rangeToSequence(string range, vector<int> &seq_group){
     vector<string> split_range;
@@ -52,7 +53,7 @@ void rangeToSequence(string range, vector<int> &seq_group){
     }
     else {
         seq_group.push_back(stoi(split_range[0]));
-        cout << seq_group[0];
+        // cout << seq_group[0];
     }
 
 
@@ -60,12 +61,35 @@ void rangeToSequence(string range, vector<int> &seq_group){
 
 // This function returns true if one of the groups is completely overlapped by the other. False otherwise
 int check_for_overlap(vector<int> group_one, vector<int> group_two){
-    bool contains = includes(group_one.begin(), group_one.end(), group_two.begin(), group_two.end());
-    return (includes(group_one.begin(), group_one.end(), group_two.begin(), group_two.end()));
+    // set longer range to group_one and shorter range to group_two
+    vector<int> container_one;
+    vector<int> container_two;
+    if (group_one.size() > group_two.size()){
+        container_one = group_one;
+        container_two = group_two;
+
+    }
+    else {
+        container_one = group_two;
+        container_two = group_one;
+    }
+
+    bool contains = includes(container_one.begin(), container_one.end(), container_two.begin(), container_two.end());
+    return (includes(container_one.begin(), container_one.end(), container_two.begin(), container_two.end()));
 }
 
 
-// Contains tests
+// String parsing tests
+int test_split_string_first_half_comma(){
+    vector<string> split_string = splitString("2-4,6-8", ","); 
+    return (split_string[0]=="2-4");     
+}
+int test_split_string_sec_half_comma(){
+    vector<string> split_string = splitString("2-4,6-8", ","); 
+    return (split_string[1]=="6-8");   
+}
+
+// Contains? tests
 int test_fully_contains(){
     vector<int> group_one = {2,3,4,5,6,7,8};
     vector<int> group_two = {3,4,5,6,7};
@@ -86,18 +110,14 @@ int test_single_group_contains(){
     vector<int> group_two = {6};
     return check_for_overlap(group_one, group_two);
 } 
-
-// String parsing tests
-int test_split_string_first_half_comma(){
-    vector<string> split_string = splitString("2-4,6-8", ","); 
-    return (split_string[0]=="2-4");     
-}
-int test_split_string_sec_half_comma(){
-    vector<string> split_string = splitString("2-4,6-8", ","); 
-    return (split_string[1]=="6-8");   
+int test_fully_contains_short_first(){
+    vector<int> group_one = {3,4,5,6,7};
+    vector<int> group_two = {2,3,4,5,6,7,8};
+    return check_for_overlap(group_one, group_two);
 }
 
-//Range to sequence tests
+
+// Range-of-numbers to sequence-of-numbers tests
 int test_range_to_sequence(){
     first_range = {};
     rangeToSequence("2-4", first_range); 
@@ -126,23 +146,43 @@ int test_range_to_sequence_two_to_three(){
 
 int main(){
     assert(test_fully_contains());
-    assert(test_single_group_contains());
     assert(test_overlapping_does_not_contain());
     assert(test_does_not_contain());
+    assert(test_single_group_contains());
+    assert(test_fully_contains_short_first());
+
     assert(test_split_string_first_half_comma());
     assert(test_split_string_sec_half_comma());
+
     assert(test_range_to_sequence());
     assert(test_range_to_sequence_equal_ends());
-    // assert(test_range_to_sequence_two_to_three());
-    // test_parsed_input_single_digits();
-    // assert(test_parsed_input_single_digits());
-    // assert(test_parsed_input_double_digits());
-    // assert(test_parsed_input_triple_digits());
+    assert(test_range_to_sequence_two_digits());
+    assert(test_range_to_sequence_two_to_three());
 
-    // assert(test_fully_contains());
-    // assert(test_overlapping());
-    // assert(test_does_not_contain());
+    
+    int elf_counter = 0;
 
+    string line;
+    vector<int> group_one;
+    vector<int> group_two;
 
+    ifstream input_file ("/home/aliu/Self_Practice_Study/AdventCalendar/advent_calendar_2022/inputs/day_4.txt");
+    if(input_file.is_open()){
+        while(getline(input_file, line)){
+            //split current line into the two string ranges
+            vector<string> split_line = splitString(line, ",");
+            
+            //assign the vectors group_one and group_two to the sequence defined by the ranges in split_line
+            rangeToSequence(split_line[0], group_one);
+            rangeToSequence(split_line[1], group_two);
+
+            if(check_for_overlap(group_one, group_two)){
+                elf_counter++;
+            }
+            group_one = {};
+            group_two = {};
+        }
+    }
+    cout << "There are " << elf_counter << " groups that overlap";
     return 0;
 }
