5cd01540d0c552526fca61d652793e2545a5b6db
Allen Liu <aliu@nauticusrobotics.com>
2023-03-08 14:25:10 -0600

    Adding Cmake

---
+++ advent_calendar_2022/build/CMakeFiles/day_01_1.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/aliu/Self_Practice_Study/AdventCalendar/advent_calendar_2022/src/day_1/day_1_puzzle_1.cpp" "CMakeFiles/day_01_1.dir/src/day_1/day_1_puzzle_1.cpp.o" "gcc" "CMakeFiles/day_01_1.dir/src/day_1/day_1_puzzle_1.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
